78fcc10941366fee5491ca82df0b427f74abb22e
JustinGaj <jtg0825@yahoo.com>
2024-08-05 19:40:40 -0400

    finished

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# WPMProject
+# Basic WPM Processor
+Beginner independent project to calculate your average words per minute (typing speed)
+Libraries: curses, wonderwords
--- WPM.py
+++ WPM.py
@@ -1,5 +1,7 @@
 import curses
 from curses import wrapper
+import time
+from wonderwords import RandomSentence
 
 def start_screen(stdscr):
     stdscr.clear()
@@ -10,32 +12,46 @@ def start_screen(stdscr):
 
 def display_text(stdscr, target, current, wpm=0):
     stdscr.addstr(target)
+    stdscr.addstr(1, 0, f"WPM: {wpm}")
 
     for i, char in enumerate(current):
         correct_char = target[i]
         color = curses.color_pair(1)
         if char != correct_char:
             color = curses.color_pair(2)
-            
+
         stdscr.addstr(0, i, char, color)
 
 def wpm_test(stdscr):
-    target_text = "Hello world this is some test text for this app!"
+    target_text = RandomSentence().sentence()
     current_text = []
+    wpm = 0
+    start_time = time.time()
+    stdscr.nodelay(True)
 
     while True:
-        stdscr.clear() 
-        display_text(stdscr, target_text, current_text)
+        time_elapsed = max(time.time() - start_time, 1)
+        wpm = round((len(current_text) / (time_elapsed / 60)) / 5)
+
+        stdscr.clear()
+        display_text(stdscr, target_text, current_text, wpm)
         stdscr.refresh()
 
-        key = stdscr.getkey()
+        if "".join(current_text) == target_text:
+            stdscr.nodelay(False)
+            break
+
+        try:
+            key = stdscr.getkey()
+        except:
+            continue
 
         if ord(key) == 27:
             break
         if key in ("KEY_BACKSPACE", '\b', "\x7f"):
             if len(current_text) > 0:
                 current_text.pop()
-        else:
+        elif len(current_text) < len(target_text):
             current_text.append(key)
 
 def main(stdscr):
@@ -44,6 +60,12 @@ def main(stdscr):
     curses.init_pair(3, curses.COLOR_WHITE, curses.COLOR_BLACK)
 
     start_screen(stdscr)
-    wpm_test(stdscr)
+    while True:
+        wpm_test(stdscr)
+        stdscr.addstr(2, 0, "You completed the text! Press ESC to exit or any other key to continue...")
+        key = stdscr.getkey()
+
+        if ord(key) == 27:
+            break
 
 wrapper(main)
